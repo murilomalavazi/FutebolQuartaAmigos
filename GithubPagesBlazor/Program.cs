f1a58e13a7d903659338dfba46257eb0ffc94c2b
Murilo Malavazi <murilomalavazi@gmail.com>
2023-05-15 22:14:18 -0300

    first commit

---
+++ GithubPagesBlazor/Program.cs
@@ -0,0 +1,25 @@
+using GithubPagesBlazor;
+using GithubPagesBlazor.Startup;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        try
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.RegisterHttpServices();
+
+            await builder.Build().RunAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[ERRO] Erro inesperado ao inicializar a aplicação! / {ex.Message}");
+        }
+    }
+}
---
+++ GithubPagesBlazor/Startup/HttpDependencies.cs
@@ -0,0 +1,20 @@
+﻿using GithubPagesBlazor.Users;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace GithubPagesBlazor.Startup
+{
+    public static class HttpDependencies
+    {
+        public static void RegisterHttpServices(this WebAssemblyHostBuilder builder)
+        {
+            var dbApi = builder.Configuration.GetSection("DbUrl").Value;
+            var apiKey = builder.Configuration.GetSection("ApiKey").Value;
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("HttpMessageHandlers", client => { client.BaseAddress = new Uri(dbApi); });
+
+            builder.Services.AddHttpClient<UserService>("HttpMessageHandlers");
+        }
+    }
+}
